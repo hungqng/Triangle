6c5d9def02fe90455a1f1ed3dc1d2a0dec8a6111
Hung Nguyen <97866339+hungqng@users.noreply.github.com>
2022-06-13 10:27:22 -0700

    Add files via upload

--- Triangle.py
+++ Triangle.py
@@ -13,3 +13,12 @@ class Solution:
                 dp[i] = n + min(dp[i], dp[i+1])
         
         return dp[0]
+
+        # Solution 2 Recursion
+        def dfs(i, j):
+            if i == len(triangle):
+                return 0
+            lower_left = triangle[i][j] + dfs(i + 1, j)
+            lower_right = triangle[i][j] + dfs(i + 1, j + 1)
+            return min(lower_left, lower_right)
+        return dfs(0,0)
