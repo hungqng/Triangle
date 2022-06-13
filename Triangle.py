247b6fad1fe65d9b24d3ef6e9f5893790a361f73
Hung Nguyen <97866339+hungqng@users.noreply.github.com>
2022-06-13 10:21:15 -0700

    Add files via upload

---
+++ Triangle.py
@@ -0,0 +1,15 @@
+# 120. Triangle
+
+# Given a triangle array, return the minimum path sum from top to bottom.
+
+# For each step, you may move to an adjacent number of the row below. More formally, if you are on index i on the current row, you may move to either index i or index i + 1 on the next row.
+
+class Solution:
+    def minimumTotal(self, triangle: List[List[int]]) -> int:
+        dp = [0] * (len(triangle) + 1)
+        
+        for row in triangle[::-1]:
+            for i, n in enumerate(row):
+                dp[i] = n + min(dp[i], dp[i+1])
+        
+        return dp[0]
